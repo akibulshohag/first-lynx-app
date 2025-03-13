722878fe55655c7a9fbb2f800951312a1f00367d
Akibul Islam <akibulislam@arogga.net>
2025-03-13 12:44:07 +0600

    code formatted

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,19 @@
 import Container from "../components/Container.tsx";
-import '../App.css'
-import { useNavigate } from 'react-router';
+import "../App.css";
+import { useNavigate } from "react-router";
 
 const Home = () => {
-    const nav = useNavigate();
+  const nav = useNavigate();
   return (
     <Container>
       <view>
-        <text className="Title" bindtap={()=>nav('/')}>Home</text>
-        <text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod possimus ab commodi expedita, amet vel! Doloribus illo quidem quia similique.</text>
+        <text className="Title" bindtap={() => nav("/")}>
+          Home
+        </text>
+        <text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod possimus
+          ab commodi expedita, amet vel! Doloribus illo quidem quia similique.
+        </text>
       </view>
     </Container>
   );
--- src/screens/ProductList.tsx
+++ src/screens/ProductList.tsx
@@ -1,17 +1,21 @@
-import Container from "../components/Container.tsx"
-import '../App.css'
-import { useNavigate } from 'react-router';
+import Container from "../components/Container.tsx";
+import "../App.css";
+import { useNavigate } from "react-router";
 const ProductList = () => {
-    const nav = useNavigate();
-    return (
-        <Container>
-        <view>
-          <text className="Title" bindtap={()=>nav('/')}>Product List</text>
-          <text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod possimus ab commodi expedita, amet vel! Doloribus illo quidem quia similique.</text>
-        </view>
-      </Container>
-    )
-  }
-  
-  export default ProductList
-  
+  const nav = useNavigate();
+  return (
+    <Container>
+      <view>
+        <text className="Title" bindtap={() => nav("/")}>
+          Product List
+        </text>
+        <text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod possimus
+          ab commodi expedita, amet vel! Doloribus illo quidem quia similique.
+        </text>
+      </view>
+    </Container>
+  );
+};
+
+export default ProductList;
