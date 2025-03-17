d4eb9dda197e0c1873988c1f78d377e52523f7f1
Akibul Islam <akibulislam@arogga.net>
2025-03-17 12:17:03 +0600

    add: count function added with zustand state management tool

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,19 +1,23 @@
 import Container from "../components/Container.tsx";
 import "../App.css";
 import { useNavigate } from "react-router";
+import useAppStore from "../../store/useAppStore.tsx";
 
 const Home = () => {
   const nav = useNavigate();
+
+  const { count, increment, decrement } = useAppStore();
   return (
     <Container>
       <view>
         <text className="Title" bindtap={() => nav("/")}>
-          Home
+          Count Screen
         </text>
-        <text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod possimus
-          ab commodi expedita, amet vel! Doloribus illo quidem quia similique.
-        </text>
+      </view>
+      <view className="Counter">
+        <text bindtap={decrement} className="Increment">-</text>
+        <text className="TotalCount">{count}</text>
+        <text bindtap={increment} className="Increment">+</text>
       </view>
     </Container>
   );
